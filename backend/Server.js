2725281e4eae1853fc42563628fb51cff1677b63
deshini kanchana <deshinisampath@gmail.com>
2025-07-08 00:28:52 +0530

    Initial Commit

--- backend/Server.js
+++ backend/Server.js
@@ -14,13 +14,26 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => console.log("✅ MongoDB Connected"))
+    .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("Mongo Error:", err));
 
 app.use("/tmf-api/customerManagement/v5/customer", customerRoutes);
 
 app.get('/', async (_req, res) => {
-    res.send('TMF-629 Running')
+   try {
+            const response = await axios.get(`https://tmf629-production.up.railway.app/tmf-api/customerManagement/v5/customer`);
+
+            const customerList = response.data.map(c => `<li>${c.id} - ${c.name}</li>`).join("");
+
+            res.send(`
+      <h2>Customer Management API</h2>
+      <ul>${customerList}</ul>
+      <p>Data fetched from <code>/tmf-api/customerManagement/v5/customer</code></p>
+    `);
+        } catch (error) {
+            console.error('Failed to load customer data:', error.message);
+            res.status(500).send('Error loading customer data');
+        }
 });
 
 const PORT = process.env.PORT || 3000;
